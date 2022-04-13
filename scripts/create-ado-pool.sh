94859b62ca1332214d53c4527a14c0b6682a3287
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-04-13 16:28:57 +0100

    updates for pool creation and cloud-init

---
+++ scripts/create-ado-pool.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# export ADO_TOKEN=
+# export ADO_ORG=
+# export ADO_PROJECT=
+# export ADO_SERVICE_CONNECTION=
+
+project_json=$(curl -s -u ":$ADO_TOKEN" "https://dev.azure.com/$ADO_ORG/_apis/projects?api-version=7.1-preview.1")
+
+project_count=$(echo "$project_json" | jq '.count')
+
+echo "Found $project_count projects"
+
+# https://phpfog.com/using-variables-in-jq-command-line-json-parser/
+# project=$(echo "$project_json" | jq '.value | map(select(.name==env.ADO_PROJECT))')
+project=$(echo "$project_json" | jq '.value | .[] | select(.name==env.ADO_PROJECT)')
+echo "$project"
+
+# .value | .[] | select(.name=="vmss") | .id
+project_id=$(echo "$project" | jq -r '.id')
+
+echo "id for project $ADO_PROJECT: $project_id"
+
+# GET https://dev.azure.com/{organization}/{project}/_apis/serviceendpoint/endpoints?api-version=6.0-preview.4
+# GET https://dev.azure.com/{organization}/{project}/_apis/serviceendpoint/endpoints?endpointNames={endpointNames}&api-version=6.0-preview.4
+
+# url="https://dev.azure.com/$ADO_ORG/$ADO_PROJECT/_apis/serviceendpoint/endpoints?api-version=6.0-preview.4"
+url="https://dev.azure.com/$ADO_ORG/$ADO_PROJECT/_apis/serviceendpoint/endpoints?endpointNames=$ADO_SERVICE_CONNECTION&api-version=6.0-preview.4"
+
+endpoints=$(curl -s -u ":$ADO_TOKEN" "$url")
+endpoint_id=$(echo "$endpoints" | jq -r '.value | .[].id')
+
+echo "$endpoints" | jq
+echo "endpoint_id: $endpoint_id"
+
+# printf "***ENDPOINTS\n:%s" "$endpoints"
+
+# GET https://dev.azure.com/{organization}/_apis/distributedtask/elasticpools?api-version=7.1-preview.1
+
+elastic_pools=$(curl -s -u ":$ADO_TOKEN" "https://dev.azure.com/$ADO_ORG/_apis/distributedtask/elasticpools?api-version=7.1-preview.1")
+
+echo "$elastic_pools"
+
+pool_name="test"
+authorize_all_pipelines="True"
+auto_provision_project_pools="False"
+
+# GET https://dev.azure.com/{organization}/{project}/_apis/serviceendpoint/endpoints?api-version=6.0-preview.4
+
+# POST https://dev.azure.com/{organization}/_apis/distributedtask/elasticpools?poolName={poolName}&authorizeAllPipelines={authorizeAllPipelines}&autoProvisionProjectPools={autoProvisionProjectPools}&projectId={projectId}&api-version=7.1-preview.1
+url="https://dev.azure.com/$ADO_ORG/_apis/distributedtask/elasticpools?poolName=$pool_name&authorizeAllPipelines=$authorize_all_pipelines&autoProvisionProjectPools=$auto_provision_project_pools&projectId=$project_id&api-version=7.1-preview.1"
+
+echo "$url"
+# json_data='{}'
+
+# new_pool=$(curl -s -X POST -H "Content-Type: application/json" -d "$json_data" -u ":$ADO_TOKEN" "$url" )
+
+# uncommment when service endpoint is available
+echo "$PWD"
+new_pool=$(curl -s -X POST -H "Content-Type: application/json" -d @params.json -u ":$ADO_TOKEN" "$url" )
+echo "$new_pool" | jq
