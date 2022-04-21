22ce2cfdd66282c196c185d0eedf6857d781be3f
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-04-21 18:32:57 +0100

    adding comment

--- scripts/custom-data.sh
+++ scripts/custom-data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# This is mainly used to highlight that we can use cloud-init as part of the deployment process if needed.
+# the below output can be viewed in the Azure Portal under the VMSS - Instances and then viewing
+# Boot diagnostics - Serial log
+
 echo "***** cat /etc/sudoers"
 cat /etc/sudoers
 
 echo "***** cat /etc/environment"
 cat /etc/environment
+
+# See custom-data.sh.bak in this same directory as described in https://www.skidmore.co.uk/post/2022_04_20_azure_devops_vmss_agents_part2/
